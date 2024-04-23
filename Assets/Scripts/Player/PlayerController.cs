82e185da8dda95ffc6100c171b0c116893002b1f
SweetyFire <mzhelezschikov@mail.ru>
2024-04-23 12:54:26 +0600

    Player slope movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,17 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : CreatureController
 {
-    [SerializeField] private float _walkSpeed = 150f;
+    [SerializeField] private float _walkSpeed = 5f;
     [SerializeField] private float _rotationSpeed = 500f;
+    [SerializeField] private float _maxSlopeAngle = 45f;
+    [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private PlayerInput _playerInput;
     [SerializeField] private Camera _camera;
 
     private Rigidbody _rb;
-    private Vector3 _moveVelocity;
-    private Vector2 _lookInput;
+    private Vector3 _moveDirection;
+    private Vector2 _lookDirection;
     private Vector2 _mousePosition;
     private float _cameraAngle;
     private PlayerWeapon _weapon;
+
+    private bool _isGrounded;
+    private RaycastHit _groundHit;
+    private float _groundCheckOffset = 0.1f;
 
     private void Awake()
     {
@@ -24,25 +30,24 @@ public class PlayerController : CreatureController
     private void Update()
     {
         LookUpdate();
+        GroundCheckUpdate();
     }
 
     private void FixedUpdate()
     {
-        Vector3 velocity = _walkSpeed * Time.fixedDeltaTime * _moveVelocity;
-        velocity.y = _rb.velocity.y;
-        _rb.velocity = velocity;
+        MoveFixedUpdate();
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
         Vector2 input = ctx.ReadValue<Vector2>();
-        _moveVelocity = new(input.x, 0f, input.y);
-        _moveVelocity = _moveVelocity.RotateTo(0f, _cameraAngle, 0f);
+        _moveDirection = new(input.x, 0f, input.y);
+        _moveDirection = _moveDirection.RotateTo(0f, _cameraAngle, 0f);
     }
 
     public void OnLook(InputAction.CallbackContext ctx)
     {
-        _lookInput = ctx.ReadValue<Vector2>();
+        _lookDirection = ctx.ReadValue<Vector2>();
     }
 
     public void OnMousePositionChanged(InputAction.CallbackContext ctx)
@@ -55,6 +60,72 @@ public class PlayerController : CreatureController
         base.InitComponents();
         _rb = GetComponent<Rigidbody>();
         _weapon = GetComponent<PlayerWeapon>();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        float slopeAngle = GetSlopeAngle();
+        _rb.useGravity = true;
+
+        Vector3 velocity;
+        if (slopeAngle > 0f)
+        {
+            if (slopeAngle > _maxSlopeAngle)
+            {
+                velocity = Vector3.zero;
+                velocity.y = _rb.velocity.y;
+            }
+            else
+            {
+                velocity = GetSlopeMoveDirection() * _walkSpeed;
+                _rb.useGravity = false;
+            }
+        }
+        else
+        {
+            velocity = _moveDirection * _walkSpeed;
+            velocity.y = _rb.velocity.y;
+        }
+
+        _rb.velocity = velocity;
+        SpeedControl();
+    }
+
+    private bool OnSlope()
+    {
+        if (_isGrounded)
+        {
+            float angle = GetSlopeAngle();
+            return angle != 0f && angle <= _maxSlopeAngle;
+        }
+
+        return false;
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 limitedVel = _rb.velocity;
+        if (_isGrounded)
+        {
+            if (limitedVel.magnitude > _walkSpeed)
+            {
+                limitedVel = limitedVel.normalized * _walkSpeed;
+
+                if (!OnSlope())
+                    limitedVel.y = _rb.velocity.y;
+            }
+        }
+
+        _rb.velocity = limitedVel;
+    }
+
+    private void GroundCheckUpdate()
+    {
+        float groundCheckRadius = _collider.radius / 1.5f;
+        float groundCheckDistance = groundCheckRadius + _groundCheckOffset;
+        Vector3 castPos = (_groundCheckOffset + groundCheckRadius) * Vector3.up + transform.position;
+        castPos += _moveDirection * (groundCheckRadius / 2f);
+        _isGrounded = Physics.SphereCast(castPos, groundCheckRadius, Vector3.down, out _groundHit, groundCheckDistance, _groundLayer);
     }
 
     private void SetCameraAngle()
@@ -89,18 +160,57 @@ public class PlayerController : CreatureController
             }
             else
             {
-                if (_lookInput == Vector2.zero) return;
-                lookVelocity = new(_lookInput.x, 0f, _lookInput.y);
+                if (_lookDirection == Vector2.zero) return;
+                lookVelocity = new(_lookDirection.x, 0f, _lookDirection.y);
                 lookVelocity = lookVelocity.RotateTo(0f, _cameraAngle, 0f);
             }
         }
         else
         {
-            if (_moveVelocity == Vector3.zero) return;
-            lookVelocity = _moveVelocity;
+            if (_moveDirection == Vector3.zero) return;
+            lookVelocity = _moveDirection;
         }
         
         rotation = Quaternion.LookRotation(lookVelocity, Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
     }
+
+    private float GetSlopeAngle()
+    {
+        return Vector3.Angle(Vector3.up, _groundHit.normal);
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(_moveDirection, _groundHit.normal).normalized;
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        CapsuleCollider col;
+        if (_collider == null)
+            col = GetComponent<CapsuleCollider>();
+        else
+            col = _collider;
+
+        float groundCheckRadius = col.radius / 1.5f;
+        float groundCheckDistance = groundCheckRadius + _groundCheckOffset;
+        Vector3 castPos = (_groundCheckOffset + groundCheckRadius) * Vector3.up + transform.position;
+        castPos += _moveDirection * (groundCheckRadius / 2f);
+
+        if (Physics.SphereCast(castPos, groundCheckRadius, Vector3.down, out RaycastHit hit, groundCheckDistance, _groundLayer))
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(castPos, Vector3.down * hit.distance);
+            Gizmos.DrawWireSphere(castPos + Vector3.down * hit.distance, groundCheckRadius);
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(castPos, groundCheckDistance * Vector3.down);
+            Gizmos.DrawWireSphere(castPos + groundCheckDistance * Vector3.down, groundCheckRadius);
+        }
+    }
+#endif
 }
